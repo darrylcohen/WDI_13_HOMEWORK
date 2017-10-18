302a97f47131079e4cf41f605d65233758867368
darryl <darrylcohen@iinet.net.au>
2017-10-18 20:14:02 +1100

    client omdb

---
+++ Darryl Cohen/wk08/3-wed/clientOMDB/app.js
@@ -0,0 +1,23 @@
+$('button').on('click', function(){
+  // HTTParty.get("http://omdbapi.com/?s=#{@movie}&apikey=2f6435d9").parsed_response['Search']
+  var search = $('input').val()
+  var url = "http://omdbapi.com/?s=" + search + "&apikey=2f6435d9"
+  $.ajax({
+    url: url,
+    method: 'get',
+    data: {}
+  }).done(function(resp) {
+    debugger
+    $results = $('.results')
+    resp.Search.forEach(function(aSearch){
+      $h2 = $('<h2>')
+      $a = $('<a>').text(aSearch.Title)
+      $a.attr("href", aSearch.Poster)
+      $a.attr("target", "_blank")
+      $h2.append($a)
+      $results.append( $h2 )
+    })
+  }).fail(function() {
+    console.log('whatever error');
+  })
+})
